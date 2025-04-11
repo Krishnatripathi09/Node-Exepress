c30ff6b9833f2f425c6b0536edc123f7e23bec16
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-04-11 15:53:49 +0530

    feat(post):Created a Post Route in app.js

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running On Port ${PORT}`);
+app.get("/user", userAuth, (req, res) => {
+  res.send("I am user data ");
 });
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("I am user data ");
+app.post("user/post", (req, res) => {
+  console.log("Post The Data Here");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running On Port ${PORT}`);
 });
