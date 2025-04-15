0dfb5f41cf04569d1f064ce1bdd3cc7001d90e23
Krishna Tripathi <192.168.9.99kj@gmail.com>
2025-04-16 05:27:17 +0530

    feat(user): Created a POST API to create a user and a get API to view firstName, lastName, email

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const { User } = require("./models/userSchema.js");
 const app = express();
 const PORT = 3000;
 
-app.post("/user", (req, res) => {
+app.use(express.json());
+app.post("/user", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  
+  const user = await User({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  res.send("User Created Successfully");
+});
+
+app.get("/user", async (req, res) => {
+  const user = await User.find({}).select("firstName lastName email");
+
+  res.send("Users Found : " + user);
 });
 
 connectDB()
